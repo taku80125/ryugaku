ef8e0204ff0ec4a682820357beebb99f91f4b91e
Iso Takuma <tak@Iso-no-MacBook-Air.local>
2018-10-31 11:50:54 +0530

    a

--- mysite/views.py
+++ mysite/views.py
@@ -23,9 +23,17 @@ def getback(request):
 
 def contact(request):
     if request.method =="POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+
+        # first_kana = request.POST.get('first_kana')
+        # last_kana = request.POST.get('last_kana')
+
         email= request.POST.get('email')
         massage = request.POST.get('massage')
-        c=Contact(email=email,  massage=massage)
+        c=Contact(email=email,  massage=massage, first_name=first_name, last_name=last_name,
+        # first_kana=first_kana, last_kana=last_kana,
+        )
         c.save()
   
         return render(request,'mysite/getback.html')   
